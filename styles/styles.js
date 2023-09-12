c70682cd686fc35fa6566a6b1abf46e9d85e3757
Amanda <amandayufeng@gmail.com>
2023-09-12 19:52:20 -0400

    Add folders

---
+++ styles/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet,Dimensions } from 'react-native';
+import { color } from 'react-native-reanimated';
+
+//to be changed but please use these and add as needed
+export const STYLES = StyleSheet.create({
+  flex: {
+    flex: 1
+  },
+  centerContainer: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 30,
+    color: 'white'
+  },
+  heading: {
+    fontSize: 25
+  },
+
+});
+
+export const COLORS = {
+  primaryDark: '#22212c',
+  primaryLight: '#f8f8f2',
+  primaryPink: '#ff80bf',
+};
