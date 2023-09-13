1ff960fb720922c67f201aa7a651a461e429d56d
Ishaan Sid <ishaan.sid@gmail.com>
2023-09-12 20:55:53 -0400

    started working on home

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {View} from 'react-native'
 
+import Home from './pages/Home'
+
 const App = () => {
   return(
     <View>
-
+      <Home/>
     </View>
   )
 }
---
+++ pages/Home.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+import {TextInput, View, Text, Button,Image, Touchable, TouchableOpacity, ScrollView,StyleSheet,Headers} from 'react-native'
+console.log("WE OPEN ")
+const Home =()=>{
+
+    return(
+        <View>
+            {/* <Headers centerComponent ={{text: "Home", style: {color: "#fff"}}}/> */}
+            <View>
+            <Text> Home</Text>
+            </View>
+        </View>
+    )
+}
+
+export default Home;
