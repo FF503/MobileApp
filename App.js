ec4cce626fc7481995e038385a3c634e50274df9
Amrit B <sri.bollapalli@gmail.com>
2023-09-12 20:13:35 -0400

    Base App

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {Text, View} from 'react-native'
+
+const App = () => {
+
+    return (
+        <View>
+            <Text style={{fontSize:30}}>Hello</Text>
+        </View>
+    )
+}
+
+export default App;
