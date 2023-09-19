736f411c9bd2a1551763c2b8bb9758209fd05521
Nirvan Daram <novdaramn11@stu.novik12.org>
2023-09-19 10:06:21 -0400

    changes

---
+++ components/CellEvent.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import ScrollPicker from  "react-native-wheel-scrollview-picker";
+import { Stylesheet, Text, View, TextInput, StyleSheet } from 'react-native';
+
+export default function CellEvent({ detail, prop }) {
+
+    const [text, onChangeText] = React.useState('Useless Text');
+    const [number, onChangeNumber] = React.useState.apply('');
+
+    const [index, setIndex] = React.useState(0);
+    const ref = React.useRef();
+    const onValueChange = (data, selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+
+    if (prop == 'input') {
+        //Input Component
+        comp = <TextInput
+            style={Stylesheet.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder=""
+        />
+    } else if (prop == 'scroll') {
+        comp = <ScrollPicker
+            dataSource = {['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21']}
+            selectedIndex = {index}
+            //
+            //
+            //
+            onValueChange={(data, selectedIndex) => {
+                //
+            }}
+            wrapperHeight = {40}
+            wrapperBackground = '#303030'
+            itemHeight = {20}
+            highlightColor = "#d8d8d8"
+            highlightBorderWidth = {1}
+
+        />
+    } else if (prop == 'calander') {
+        comp = <Text> calander prop will be completed soon hopefully </Text>
+    } else if (prop == 'dropdown') {
+        comp = <Text> dropdown prop will be complete soon hopefully </Text>
+    }
+
+    return(
+        <View style={styles.cell}>
+            <Text style={styles.detailText}> {detail} </Text>
+            {comp}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    cell: {
+        justifyContent: 'left', 
+        alignItems: 'center', 
+        width: '94%', 
+        height: 60, 
+        borderWidth: 1, 
+        marginTop: 5, 
+        borderRadius: 10, 
+        backgroundColor: '#303030'
+    }, 
+
+    detailText: {
+        color: 'white', 
+        //
+        //
+        width: '40%', 
+        fontSize: 20, 
+        marginLeft: 5, 
+    }, 
+
+    input: {
+        height: '60%', 
+        width: '55%', 
+        borderWidth: 1, 
+        borderRadius: 10, 
+        borderWidth: 1, 
+        borderColor: '#525252', 
+        paddingLeft: 10, 
+        color: 'white'
+    },
+})
+
---
+++ pages/CreateEvent.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { StyleSheet, Text, View} from 'react-native'
+import CellEvent from '../components/CellEvent';
+
+export default function CreateEvent() {
+
+
+
+    return(
+        <View style={styles.container}>
+            <View style={styles.eventCont}>
+                <Text style={styles.eventText}> Create Event</Text>
+            </View>
+            <View style={styles.cellCont}>
+                <CellEvent detail='Event Name:' prop='input'>
+                    
+                </CellEvent>
+                <CellEvent detail='Location:' prop='input'>
+                    
+                </CellEvent>
+                <CellEvent detail='Open Seats:' prop='scroll'>
+                    
+                </CellEvent>
+                <CellEvent detail='Student Lead:' prop='input'>
+                    
+                </CellEvent>
+                <CellEvent detail='Mentor Lead:' prop='input'>
+                    
+                </CellEvent>
+                <CellEvent detail='Date:' prop='input'>
+                    
+                </CellEvent>
+                <CellEvent detail='Event Type:' prop='dropdown'>
+                    
+                </CellEvent>
+            </View>
+        </View>
+    );
+
+    
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'black', 
+        flex: 1, 
+        alignItems: 'center',
+    },
+
+    eventCont: {
+        marginTop: '12%', 
+        width: '100%',
+    },
+
+    eventText: {
+        fontSize: 40, 
+        color: 'white',
+    }, 
+
+    cellCont: {
+        width: '100%', 
+        alignItems: 'center', 
+        marginTop: 10, 
+        //
+        //
+        height: '50%'
+    },
+});
