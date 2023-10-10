0edec4975b5d94281d334d86c13a0c9154378857
Sam E <samhith.enganti@frogforce503.org>
2023-10-10 19:36:52 -0400

    updated packages, etc.

--- styles/styles.js
+++ styles/styles.js
@@ -1,27 +1,48 @@
-import { StyleSheet,Dimensions } from 'react-native';
-import { color } from 'react-native-reanimated';
+import {StyleSheet, Dimensions} from 'react-native';
+import {color} from 'react-native-reanimated';
+
+export const COLORS = {
+  primaryDark: '#22212c',
+  primaryLight: '#000000',
+  primaryPink: '#ff80bf',
+  secondaryDark: '#ff80bf',
+  secondaryLight: '#ffffff',
+  secondaryPink: '#22212c',
+  tertiaryDark: '#ffffff',
+  tertiaryLight: '#ff80bf',
+  tertiaryPink: '#000000',
+};
 
 //to be changed but please use these and add as needed
 export const STYLES = StyleSheet.create({
   flex: {
-    flex: 1
+    flex: 1,
   },
   centerContainer: {
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   title: {
     fontSize: 30,
-    color: 'white'
+    color: COLORS.primaryLight,
+    fontWeight: 'bold',
+    left: 20,
+    top: 20,
+    marginBottom: 20,
   },
-  heading: {
-    fontSize: 25
+  shopCard: {
+    width: (Dimensions.get('window').width - 40) / 2,
+    height: 100,
+    backgroundColor: COLORS.secondaryLight,
+    borderRadius: 10,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-
+  shopText: {
+    fontSize: 20,
+    color: COLORS.primaryLight,
+    alignContent: 'center',
+    textAlign: 'center',
+  },
 });
-
-export const COLORS = {
-  primaryDark: '#22212c',
-  primaryLight: '#f8f8f2',
-  primaryPink: '#ff80bf',
-};
