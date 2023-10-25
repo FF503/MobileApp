8d4c302b0f91baee46ba34e843efa82f964b0ff5
Jyotsna Swaminathan <priyaa84@gmail.com>
2023-10-24 20:31:09 -0400

    Designed login page

--- pages/Login.js
+++ pages/Login.js
@@ -1,19 +1,52 @@
 import React from 'react'
-import {View,Text, Button} from 'react-native'
+import {View,Text, Button, Image} from 'react-native'
 const login = () =>{
   console.log("Username")
 }
 
 const Login = () => {
-  
-    const x = 1+1
+
       return(
-    <View>
-    <Button
-    onPress={login}
-    title='Login'
-    color='#2196F3'
-    />
+    <View
+    style={{flex:1, backgroundColor:'lightgreen'}}
+    >
+      <View
+        style={{ flex:1,alignContent:"center",fontSize:20, flexDirection:'column-reverse'}}>
+        <Text
+          style={{color:'darkgreen', textAlign:"center",fontSize:200}}>
+            Lilypad
+        </Text>
+      </View>
+      <View style={{flex:1,  alignContent:"center"}}>
+        <Text
+          style={{color:'black', textAlign:"center",fontSize:40}}>
+            Login
+        </Text>
+        
+        <View style={{flex:1,  alignContent:"center", flexDirection: "row"}}>
+          <View  style={{flex:1,  alignContent:"center"}}>
+          </View>
+          <View  style={{flex:1,  alignContent:"center"}}>
+          <Button style={{alignContent:"center",flex:4,fontSize:40}}
+            onPress={login}  
+            title='Sign in'
+        /> 
+          </View>
+       
+
+        <View  style={{flex:1,  alignContent:"center"}}>
+          </View>
+        </View>
+        
+     <View style={{flex:6,  alignContent:"center"}}>
+        <Text
+          style={{color:'black', textAlign:"center",fontSize:20}}>
+            with Outlook
+        </Text>
+        </View>
+      </View>
+     
+   
     </View>
   )
 }
