738774e36a60c5dd4ac49f8fffb5ce696b1144d5
NirvanD <novdaramn11@stu.novik12.org>
2023-11-04 21:21:25 -0400

    changes some styles and tried to fix functionality of qr code scanner

--- pages/AttendanceCode.js
+++ pages/AttendanceCode.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   Text,
   View,
   Linking,
-  TouchableHighlight,
   PermissionsAndroid,
   Platform,
   StyleSheet,
@@ -16,123 +14,23 @@ import {
   ScrollView,
   Dimensions
 } from 'react-native';
-//import { Table, Row, Rows, Col, TableWrapper } from 'react-native-table-component';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-reanimated-table';
 import ProgressCircle from 'react-native-progress-circle'
 import Accordion from 'react-native-collapsible/Accordion';
-//import DataTable from 'react-data-table-component';
-
-//import {CameraKitCameraScreen} from 'react-native-camera-kit';
-//import { CameraScreen } from 'react-native-camera-kit';
+import { useCameraDevices } from 'react-native-vision-camera';
+import { Camera } from 'react-native-vision-camera';
+import { useScanBarcodes, BarcodeFormat } from 'vision-camera-code-scanner';
 
 const AttendanceCode = () => {
-
-  const [button, setdb] = useState([0, 0, 0, 0, 0, 0, 0]);
-  const [b1, setB1] = useState(1);
-  //require("../images/arrow.png")
-  const dropdownTitle = (props) => {
-    return (
-      <SafeAreaView>
-        <View>
-          <TouchableOpacity
-            style={{}}
-            onPress={async () => {
-              let temp = b1
-              if (temp[v] == 0) {
-                temp[v] = 1
-              } else {
-                temp[v] = 0
-              }
-              setB1(temp)
-              console.log(temp)
-            }}
-          >
-            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-              <Text style={[styles.text, { width: '90%', color: 'black', fontWeight: 'bold' }]}>{props}</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-    )
-  }
-
   const [qrvalue, setQrvalue] = useState('');
   const [opneScanner, setOpneScanner] = useState(false);
   const [number, onChangeNumber] = useState('');
-  const [data, setData] = useState({
+  const [tableData, setTableData] = useState({
     tableHead: ['', 'Requirement', 'Goal', 'Attended', 'Possible', 'Percent', 'Status'],
     tableHead2: ['Event Date', 'Event Title', 'Cutoff Time', 'Checkin Time', 'Attended'],
-    tableTitle: [[dropdownTitle('Team Meeting', 0)], [dropdownTitle('FIRST \nWorkshops', 1)], [dropdownTitle('Community Outreach', 2)], [dropdownTitle('Community Service', 3)], [dropdownTitle('Off Season Competition', 4)], [dropdownTitle('YPP Training', 5)], [dropdownTitle('Saftey Training', 6)]],
-    //flexArray: [75, 25, 55, 10, 90, 90, 75],
-    flexArray: [1, 1, 1, 1, 1, 1, 1],
-    flexArray2: [48, 13, 20, 20, 20, 50],
     widthArray: [130, 100, 50, 80, 80, 80,],
     widthArray2: [104, 104, 104, 104, 104],
     widthArray3: [100, 50, 80, 80, 80,],
-  })
-
-  const onOpenlink = () => {
-    // If scanned then function to open URL in Browser
-    Linking.openURL(qrvalue);
-  };
-
-  // const onBarcodeScan = (qrvalue) => {
-  //   // Called after te successful scanning of QRCode/Barcode
-  //   setQrvalue(qrvalue);
-  //   setOpneScanner(false);
-  // };
-
-  const onBarcodeScan = (qrvalue) => {
-    // Called after te successful scanning of QRCode/Barcode
-    setQrvalue(qrvalue);
-    setOpneScanner(false);
-
-    Alert.alert(
-      "Your attendance has been recoreded",
-      "",
-      [
-        {
-          text: "Ok",
-          style: "ok",
-        },
-      ],
-    );
-  };
-
-  const onOpneScanner = () => {
-    // To Start Scanning
-    if (Platform.OS === 'android') {
-      async function requestCameraPermission() {
-        try {
-          const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.CAMERA,
-            {
-              title: 'Camera Permission',
-              message: 'App needs permission for camera access',
-            },
-          );
-          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            // If CAMERA Permission is granted
-            setQrvalue('');
-            setOpneScanner(true);
-          } else {
-            alert('CAMERA permission denied');
-          }
-        } catch (err) {
-          alert('Camera permission err', err);
-          console.warn(err);
-        }
-      }
-      // Calling the camera permission function
-      requestCameraPermission();
-    } else {
-      setQrvalue('');
-      setOpneScanner(true);
-    }
-  };
-  const [header, setHeader] = useState({
-    data: [['Requirement', 'Goal', 'Attended', 'Possible', 'Percent', 'Status']],
-    data2: [['Event Date', 'Event Title', 'Cutoff Time', 'Checkin Time', 'Attended']],
   })
   const [list, setList] = useState(
     {
@@ -153,7 +51,6 @@ const AttendanceCode = () => {
   const [activeSections, setActiveSections] = useState([])
 
   const renderHeader = section => {
-    console.log('state', section);
     let index = activeSections[0];
     let currentObj = list.section[index];
     return (
@@ -163,8 +60,10 @@ const AttendanceCode = () => {
             <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
               <TableWrapper style={{ flexDirection: 'row' }}>
                 <Col data={[(
-                  <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={[styles.text, { fontWeight: 'bold' }]}>{section.tableTitle}</Text>
+                  <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+                    <View style={{ width: '80%' }}>
+                      <Text style={[styles.text, { fontWeight: 'bold' }]}>{section.tableTitle}</Text>
+                    </View>
                     <Image
                       style={{ height: 25, width: 25 }}
                       source={
@@ -180,7 +79,7 @@ const AttendanceCode = () => {
                       }
                     />
                   </View>)]} style={styles.title} width={130} heightArr={[43]} textStyle={styles.text} />
-                <Row data={section.tableHead[0]} textStyle={styles.text} widthArr={data.widthArray3} height={43} />
+                <Row data={section.tableHead[0]} textStyle={styles.text} widthArr={tableData.widthArray3} height={43} />
               </TableWrapper>
             </Table>
           </View>
@@ -193,103 +92,126 @@ const AttendanceCode = () => {
     return (
       <View style={styles.content}>
         <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
-          <Row data={data.tableHead2} textStyle={[styles.text, { fontWeight: 'bold' }]} widthArr={data.widthArray2} height={43} />
-          <Rows data={section.tableData} textStyle={styles.text} widthArr={data.widthArray2} />
+          <Row data={tableData.tableHead2} textStyle={[styles.text, { fontWeight: 'bold' }]} widthArr={tableData.widthArray2} height={43} />
+          <Rows data={section.tableData} textStyle={styles.text} widthArr={tableData.widthArray2} />
         </Table>
       </View>
     );
   };
 
   const updateSections = activeSections2 => {
-    console.log(activeSections2)
     let temp = activeSections2
-    console.log(temp)
     setActiveSections(temp)
-    //setActiveSections({ activeSections2 });
   };
+
+  const [hasPermission, setHasPermission] = React.useState(false);
+  const devices = useCameraDevices();
+  const device = devices.back;
+
+  const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE], {
+    checkInverted: true,
+  });
+
+  // Alternatively you can use the underlying function:
+  //
+  // const frameProcessor = useFrameProcessor((frame) => {
+  //   'worklet';
+  //   const detectedBarcodes = scanBarcodes(frame, [BarcodeFormat.QR_CODE], { checkInverted: true });
+  //   runOnJS(setBarcodes)(detectedBarcodes);
+  // }, []);
+
+  useEffect(() => {
+    (async () => {
+      const status = await Camera.requestCameraPermission();
+      setHasPermission(status === 'authorized');
+    })();
+  }, []);
+
   return (
     <SafeAreaView style={{}}>
-      <ScrollView>
-        <View>
-
-        </View>
-        <View style={{ flexDirection: 'column', margin: 10 }}>
-          <View style={{ flexDirection: 'row', }}>
-            <Text style={{ flex: 3.6, fontSize: 35, color: 'black' }}>Attendance</Text>
-            <TouchableOpacity style={{ flex: 1 }}
-              onPress={async () => {
-                setOpneScanner(true)
-              }}
-            >
-              <Image style={{ width: 50, height: 50 }} source={require("../images/cameraIconPink.png")} />
-            </TouchableOpacity>
-          </View>
-          <View style={{ height: 10 }}></View>
-          <View style={{ flexDirection: 'row', }}>
-            <TextInput
-              style={[styles.input, { flex: 2 }]}
-              onChangeText={onChangeNumber}
-              value={number}
-              placeholder="useless placeholder"
-              keyboardType="numeric"
-            />
-            <View style={{ flex: 0.5 }}></View>
-            <TouchableOpacity style={{ flex: 1, borderWidth: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'pink' }}>
-              <Text style={{ color: 'black', fontSize: 20 }}>Submit</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={{ height: 10 }}></View>
-          <Text style={{ fontSize: 35, color: 'black' }}>Roster Requirements</Text>
-          <View style={{ flexDirection: 'row', }}>
-            <Text style={{ fontWeight: 'bold', fontSize: 20, color: 'black' }}>Name: </Text>
-            <Text style={{ fontSize: 20, color: 'black' }}>Anish Manda</Text>
-          </View>
-          <View style={{ flexDirection: 'row', }}>
-            <Text style={{ fontWeight: 'bold', fontSize: 20, color: 'black' }}>Assigned Group: </Text>
-            <Text style={{ fontSize: 20, color: 'black' }}>Web</Text>
-          </View>
-          <View style={{ height: 10 }}></View>
-          <View style={styles.circle}>
-            <ProgressCircle
-              percent={48}
-              radius={100}
-              borderWidth={8}
-              color="#3399FF"
-              shadowColor="#999"
-              bgColor="#fff">
-              <Text style={{ fontSize: 22, color: 'black' }}>{'48% Complete'}</Text>
-            </ProgressCircle>
-          </View>
-          <ScrollView horizontal={true}>
-            {/* <View>
-              <Table borderStyle={{ borderWidth: 1, borderColor: '' }}>
-                <Row data={data.tableHead} style={[styles.head, { backgroundColor: 'pink' }]} height={43} widthArr={data.widthArray} textStyle={styles.text} />
-                <TableWrapper style={styles.wrapper}>
-                  <Col data={data.tableTitle[0]} style={styles.title} width={130} heightArr={[43, 43, 43, 43, 43, 43, 43]} textStyle={styles.text} />
-                  <Rows data={data.tableData} style={styles.row} height={43} widthArr={data.widthArray2} textStyle={styles.text} />
-                </TableWrapper>
-                <Rows data={data.tableData1} style={[styles.row, { backgroundColor: 'lightgrey'}]} height={43} widthArr={data.widthArray} textStyle={styles.text} />
-              </Table>
-            </View> */}
-            <View style={{}}>
-              <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff', padding: 2 }}>
-                <Row data={data.tableHead} textStyle={[styles.text, { fontWeight: 'bold' }]} widthArr={data.widthArray} height={43} />
-              </Table>
-              <Accordion
-                sections={list.section}
-                activeSections={activeSections}
-                renderHeader={renderHeader}
-                renderContent={renderContent}
-                onChange={updateSections}
-                duration={500}
-                underlayColor="#fff"
-                containerStyle={{ marginLeft: 0 }}
-              />
+      {opneScanner && device != null && hasPermission ? (
+        <>
+          <Camera
+            style={StyleSheet.absoluteFill}
+            device={device}
+            isActive={true}
+            frameProcessor={frameProcessor}
+            frameProcessorFps={5}
+          />
+          {barcodes.map((barcode, idx) => (
+            <Text key={idx} style={styles.barcodeTextURL}>
+              {barcode.displayValue}
+            </Text>
+          ))}
+        </>
+      ) : (
+        <ScrollView>
+          <View style={{ width: Dimensions.get('window').width, marginTop: 10 }}>
+            <View style={{ flexDirection: 'row', width: Dimensions.get('window').width - 10, justifyContent: 'space-between', marginLeft: 10 }}>
+              <Text style={{ fontSize: 35, color: 'black' }}>Attendance</Text>
+              <TouchableOpacity style={{ marginRight: 5 }}
+                onPress={async () => {
+                  setOpneScanner(true)
+                }}
+              >
+                <Image style={{ width: 50, height: 50 }} source={require("../images/cameraIconPink.png")} />
+              </TouchableOpacity>
             </View>
-          </ScrollView>
-        </View>
-      </ScrollView>
-      {/* )} */}
+            <View style={{ height: 10 }}></View>
+            <View style={{ flexDirection: 'row', width: Dimensions.get('window').width, justifyContent: 'space-between' }}>
+              <TextInput
+                style={[styles.input, { marginLeft: 10 }]}
+                onChangeText={onChangeNumber}
+                value={number}
+                placeholder="Attendance Code"
+                keyboardType="numeric"
+              />
+              <TouchableOpacity style={{ borderWidth: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'pink', marginRight: 10 }}>
+                <Text style={{ color: 'black', fontSize: 20, paddingTop: 5, paddingBottom: 5, paddingLeft: 10, paddingRight: 10 }}>Submit</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={{ height: 10 }}></View>
+            <Text style={{ fontSize: 35, color: 'black', marginLeft: 10 }}>Roster Requirements</Text>
+            <View style={{ flexDirection: 'row', }}>
+              <Text style={{ fontWeight: 'bold', fontSize: 20, color: 'black', marginLeft: 10 }}>Name: </Text>
+              <Text style={{ fontSize: 20, color: 'black' }}>Anish Manda</Text>
+            </View>
+            <View style={{ flexDirection: 'row', }}>
+              <Text style={{ fontWeight: 'bold', fontSize: 20, color: 'black', marginLeft: 10 }}>Assigned Group: </Text>
+              <Text style={{ fontSize: 20, color: 'black' }}>{qrvalue}</Text>
+            </View>
+            <View style={{ height: 10 }}></View>
+            <View style={styles.circle}>
+              <ProgressCircle
+                percent={48}
+                radius={100}
+                borderWidth={8}
+                color="#3399FF"
+                shadowColor="#999"
+                bgColor="#fff">
+                <Text style={{ fontSize: 22, color: 'black' }}>{'48% Complete'}</Text>
+              </ProgressCircle>
+            </View>
+            <ScrollView horizontal={true}>
+              <View style={{ marginLeft: 10, marginRight: 10 }}>
+                <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff', padding: 2 }}>
+                  <Row data={tableData.tableHead} textStyle={[styles.text, { fontWeight: 'bold' }]} widthArr={tableData.widthArray} height={43} />
+                </Table>
+                <Accordion
+                  sections={list.section}
+                  activeSections={activeSections}
+                  renderHeader={renderHeader}
+                  renderContent={renderContent}
+                  onChange={updateSections}
+                  duration={500}
+                  underlayColor="#fff"
+                  containerStyle={{ marginLeft: 0 }}
+                />
+              </View>
+            </ScrollView>
+          </View>
+        </ScrollView>
+      )}
     </SafeAreaView>
   );
 };
@@ -307,13 +229,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginTop: 20
   },
-  // textStyle: {
-  //   color: 'black',
-  //   fontSize: 16,
-  //   textAlign: 'center',
-  //   padding: 10,
-  //   marginTop: 16,
-  // },
   buttonStyle: {
     fontSize: 16,
     color: 'white',
@@ -332,9 +247,9 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    //margin: 12,
     borderWidth: 1,
     padding: 10,
+    width: Dimensions.get('window').width * .6
   },
   circle: {
     justifyContent: "center",
@@ -342,12 +257,6 @@ const styles = StyleSheet.create({
     marginTop: 25,
     marginBottom: 25
   },
-  // container: { flex: 1, padding: 16, paddingTop: 30 },
-  // row: { backgroundColor: '' },
-  // head: { backgroundColor: '', },
-  // text: { margin: 2, color: 'black', fontSize: 15, textAlign: 'center' },
-  // wrapper: { flexDirection: 'row' },
-  // dataWrapper: { marginTop: -1 },
   text: { padding: 2.5, color: 'black', justifyContent: 'center', textAlign: 'center' },
   content: {
     backgroundColor: '#FFFFFF',
@@ -359,7 +268,6 @@ const styles = StyleSheet.create({
   },
   header: {
     backgroundColor: '#FFFFFF',
-    //marginTop: 1,
     paddingTop: 2,
     paddingBottom: 2,
     flexDirection: 'row',
